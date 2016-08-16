11d027c3a034bed91a01104a66e952ab8ad9d93f
rohan kalra <rohan kalra>
2016-08-16 18:59:52 +0530

    none

--- app/src/main/java/rohan/srmadt/Users.java
+++ app/src/main/java/rohan/srmadt/Users.java
@@ -7,49 +7,61 @@ import android.os.AsyncTask;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Users extends AppCompatActivity {
 
     private static String urlString;
-    private TextView tv1;
+    public TextView tv1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
-        tv1=(TextView) findViewById(R.id.Tv);
+        tv1 = (TextView) findViewById(R.id.Tv);
         tv1.setText("");
         urlString = "http://jsonplaceholder.typicode.com/users";
         new ProcessJSON().execute(urlString);
 
     }
-        private class ProcessJSON extends AsyncTask<String, Void, String> {
-            protected String doInBackground(String... strings) {
-                String stream = null;
-                String urlString = strings[0];
 
-                HTTPHandler hh = new HTTPHandler();
-                stream = hh.GetHTTPData(urlString);
-                return stream;
+    private class ProcessJSON extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... strings) {
+            String stream = null;
+            String urlString = strings[0];
+            HTTPHandler hh = new HTTPHandler();
+            stream = hh.GetHTTPData(urlString);
+            return stream;
+        }
 
-            }
-            @Override
-            protected void onPostExecute(String stream) {
-                if (stream!=null) {
-                    try {
-                        JSONObject reader = new JSONObject(stream);
+        @Override
+        protected void onPostExecute(String stream) {
+            tv1.setText("reached");
+            if (stream != null) {
+                tv1.setText("reached 2");
+                try {
+                    tv1.setText(tv1.getText() + "We are processing the JSON data....\n\n");
+                    JSONArray jsonArray = new JSONArray(stream);
+                        /*JSONObject reader = new JSONObject(stream);
                         String id = reader.getString("id");
                         String name = reader.getString("name");
                         String username = reader.getString("username");
-                        tv1.setText(tv1.getText()+ "We are processing the JSON data....\n\n");
+
                         tv1.setText(tv1.getText() + id);
                         tv1.setText(tv1.getText() + name);
                         tv1.setText(tv1.getText() + username);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                    */
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String name = jsonObject.getString("name");
+                        tv1.setText(tv1.getText() + name);
                     }
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
             }
         }
     }
+}
