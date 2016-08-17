19b91a5662d08f92d463e1c86277354810b29f69
Pratik Saxena <pratikbsp@yahoo.com>
2016-08-17 23:56:40 +0530

    fixed issues

--- app/src/main/java/rohan/srmadt/Photos.java
+++ app/src/main/java/rohan/srmadt/Photos.java
@@ -14,6 +14,9 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,6 +25,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class Photos extends AppCompatActivity {
 
@@ -32,7 +36,7 @@ public class Photos extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_posts);
+        setContentView(R.layout.activity_photos);
         tv1 = (TextView) findViewById(R.id.Tv);
         imageView=(ImageView) findViewById(R.id.imageView);
         tv1.setText(tv1.getText() + "\n\n");
@@ -81,8 +85,8 @@ public class Photos extends AppCompatActivity {
                         String url1 = jsonObject.getString("url");
                         String url = jsonObject.getString("thumbnailUrl");
                         tv1.setText(tv1.getText() +"\n Title "+title+"\n id "+id+"\n AlbumId " + albumid + "\n Id " + id + "\n Url " + url1 + "\n\n");
-                        imageView.setImageBitmap(getBitmapFromURL(url));
-
+                        //imageView.setImageBitmap(getBitmapFromURL(url));
+                        new GetBitmap().execute(url);
                     //}
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -90,4 +94,34 @@ public class Photos extends AppCompatActivity {
             }
         }
     }
+
+    private class GetBitmap extends AsyncTask<String, Void, Bitmap> {
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                String string = strings[0];
+                Log.e("src", string);
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                HttpGet httpGet = new HttpGet(string);
+
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+                InputStream imgStream = httpResponse.getEntity().getContent();
+                Bitmap myBitmap = BitmapFactory.decodeStream(imgStream);
+                Log.e("Bitmap", "returned");
+                return myBitmap;
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("Exception", e.getMessage());
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap stream) {
+            if(stream != null)
+            imageView.setImageBitmap(stream);
+        }
+
+    }
+
+
 }
